6d83803e79b75ce9f2e6f5e1f385e9d2860c8d3e
Jeremy Muhlich <jmuhlich@bitflood.org>
2015-04-03 15:54:46 -0400

    added script to extract literal blocks from a sphinx doc

---
+++ src/extract_model.py
@@ -0,0 +1,18 @@
+import sys
+import docutils.examples
+
+def extract_literals(node):
+    if node.tagname == 'literal_block':
+        text = node.astext()
+    else:
+        text = ''
+        for child in node.children:
+            text += extract_literals(child)
+    return text
+
+with open(sys.argv[1]) as f:
+    input_string = f.read().decode('utf-8')
+settings = {'report_level': 5}
+doc, publisher = docutils.examples.internals(input_string,
+                                             settings_overrides=settings)
+print extract_literals(doc)
