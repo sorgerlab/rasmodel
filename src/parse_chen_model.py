e74fd81d4925b4260d8897b458fa9dd520c617bd
Jeremy Muhlich <jmuhlich@bitflood.org>
2015-04-23 12:48:00 -0400

    added script for parsing and exploring chen 2009 erbb model

---
+++ src/parse_chen_model.py
@@ -0,0 +1,82 @@
+import sys
+import collections
+import re
+import itertools
+import lxml.etree
+
+Species = collections.namedtuple('Species',
+                                 'id name label compartment')
+
+class Reaction(collections.namedtuple(
+        'ReactionBase',
+        'id name label reactants products')):
+    def __repr__(self):
+        simple_attrs = ('id', 'name', 'label')
+        simple_attr_repr = ', '.join('{0}={1!r}'.format(a, getattr(self, a))
+                                     for a in simple_attrs)
+        species_repr = ', '.join(
+            ['{0}=({1})'.format(et, ', '.join(s.name for s in getattr(self, et)))
+             for et in 'reactants', 'products'])
+        return '{0}({1}, {2})'.format(type(self).__name__,
+                                      simple_attr_repr, species_repr)
+
+def xpath(obj, path, single=True):
+    result = map(unicode, obj.xpath(path, namespaces={'s': ns}))
+    if single:
+        if len(result) == 0:
+            result = None
+        elif len(result) == 1:
+            result = result[0]
+        else:
+            raise ValueError("XPath expression matches more than one value")
+    return result
+
+def reactionSpecies(element, entity_type):
+    entity_type = entity_type.title()
+    query = 's:listOf{}s/s:speciesReference/@species'.format(entity_type)
+    return xpath(element, query, single=False)
+
+ns = 'http://www.sbml.org/sbml/level2'
+qnames = dict((tag, lxml.etree.QName(ns, tag).text)
+              for tag in ('species', 'reaction'))
+
+sbml_file = open(sys.argv[1])
+
+all_species = []
+all_reactions = []
+species_id_map = {}
+for event, element in lxml.etree.iterparse(sbml_file, tag=qnames['species']):
+    species_id = element.get('id')
+    name = element.get('name')
+    label = xpath(element, 's:notes/text()').strip()
+    # special fixup for weird ATP label
+    if label == 'ATP  1.2e9':
+        label = 'ATP'
+    compartment = xpath(element, 's:annotation/text()')
+    if compartment is not None:
+        compartment = compartment.strip().lower()
+    s = Species(species_id, name, label, compartment)
+    all_species.append(s)
+    species_id_map[s.id] = s
+    if s.name in globals():
+        raise RuntimeError('duplicate component name: {}'.format(s.name))
+    globals()[s.name] = s
+sbml_file.seek(0)
+for event, element in lxml.etree.iterparse(sbml_file, tag=qnames['reaction']):
+    rxn_id = element.get('id')
+    label = re.sub(r' +', ' ', element.get('name'))
+    name, label = label.split(' ', 1)
+    reactants = tuple(map(species_id_map.get, reactionSpecies(element, 'reactant')))
+    products = tuple(map(species_id_map.get, reactionSpecies(element, 'product')))
+    r = Reaction(rxn_id, name, label, reactants, products)
+    all_reactions.append(r)
+    if r.name in globals():
+        raise RuntimeError('duplicate component name: {}'.format(r.name))
+    globals()[r.name] = r
+
+# determine truly redundant species -- same name, same compartment
+sa = sorted(all_species, key=lambda s: (s.label, s.compartment))
+rs = [x
+      for x in ((n, map(lambda s: s.compartment, it))
+          for n, it in itertools.groupby(sa, lambda s: s.label))
+      if len(x[1]) == 2 and x[1][0] == x[1][1]]
