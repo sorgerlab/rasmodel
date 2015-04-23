360e41681535bf889b338af02a9cbf302235462a
Jeremy Muhlich <jmuhlich@bitflood.org>
2015-04-23 14:12:32 -0400

    beginning of code to parse species names to detect "rules"

--- src/parse_chen_model.py
+++ src/parse_chen_model.py
@@ -84,3 +84,8 @@ rs = [x
       for x in ((n, map(lambda s: s.compartment, it))
           for n, it in itertools.groupby(sa, lambda s: s.label))
       if len(x[1]) == 2 and x[1][0] == x[1][1]]
+
+# find reactions where product is not a trivial concatenation of reactants
+# (e.g. A + B -> A:B)
+mismatch_rxns = [r for r in all_reactions
+                 if r.products[0].label != ':'.join([s.label for s in r.reactants])]
