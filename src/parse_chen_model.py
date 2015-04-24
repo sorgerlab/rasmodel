385b9295bdd73352878794248f1fbc1cb45a6c55
Jeremy Muhlich <jmuhlich@bitflood.org>
2015-04-24 15:17:30 -0400

    start adding clusters

--- src/parse_chen_model.py
+++ src/parse_chen_model.py
@@ -2,6 +2,7 @@ import sys
 import collections
 import re
 import itertools
+import operator
 import lxml.etree
 import pygraphviz
 
@@ -39,6 +40,12 @@ def reactionSpecies(element, entity_type):
     entity_type = entity_type.title()
     query = 's:listOf{}s/s:speciesReference/@species'.format(entity_type)
     return xpath(element, query, single=False)
+
+def species_by_label(pattern, multi=False):
+    if isinstance(pattern, basestring):
+        pattern = r'^{}$'.format(re.escape(pattern))
+    gen = (s for s in species if re.search(pattern, s.label))
+    return list(gen) if multi else next(gen)
 
 ns = 'http://www.sbml.org/sbml/level2'
 qnames = dict((tag, lxml.etree.QName(ns, tag).text)
@@ -91,14 +98,45 @@ rs = [x
 mismatch_rxns = [r for r in reactions
                  if r.products[0].label != ':'.join([s.label for s in r.reactants])]
 
-graph = pygraphviz.AGraph(directed=True)
+cluster_seq = itertools.count()
+def add_cluster(*species_list):
+    nodes = [s.id for s in species_list]
+    name = 'cluster_{}'.format(next(cluster_seq))
+    graph.add_subgraph(nodes, name, color='none', bgcolor='gray97')
+
+graph = pygraphviz.AGraph(directed=True, rankdir='LR')
+graph.node_attr.update(fontname='Helvetica')
 for s in species:
-    graph.add_node(s.id, label=s.label, shape='none')
+    graph.add_node(s.id, _type='species', label=s.label, shape='none', bgcolor='white', margin=0)
 for r in reactions:
-    graph.add_node(r.id, label=r.name)
+    graph.add_node(r.id, _type='reaction', label=r.name, shape='none', fontcolor='#13ac4a')
     for reactant in r.reactants:
         graph.add_edge(reactant.id, r.id)
     for product in r.products:
         graph.add_edge(r.id, product.id)
-graph.add_subgraph([c141.id, c140.id, c143.id, c531.id, c288.id, c117.id], name='cluster_1')
+
+# single receptors and 2:3 / 2:4 dimers
+add_cluster(c141, c140, c143, c531, c288, c117)
+# ligands
+add_cluster(c1, c514)
+# ligand-bound single receptors
+add_cluster(c3, c142, c144)
+# ligand-bound receptor dimers
+add_cluster(c4, c145, c146, c147, c355, c345, c516, c517)
+# ATP-and-ligand-bound receptor dimers
+add_cluster(c116, c122, c127, c128, c168, c139, c137, c138)
+
+# delete some "nuisance" nodes from the graph
+#for nuisance in ('ATP', 'R_degraded', 'Inh'):
+#   graph.remove_node(species_by_label(nuisance).id)
+
+clustered_species = [n for g in graph.subgraphs() for n in g.nodes()]
+cs_edges = graph.edges(clustered_species)
+nodes_keep = set(reduce(operator.add, map(list, cs_edges), []))
+nodes_drop = set(graph.nodes()) - nodes_keep
+nd_edges = graph.edges(nodes_drop)
+nodes_drop.update(set(reduce(operator.add, map(list, nd_edges), [])))
+for n in list(nodes_drop):
+    graph.remove_node(n)
+
 graph.write('chen_2009.dot')
