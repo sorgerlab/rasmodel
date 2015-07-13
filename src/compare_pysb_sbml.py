c73b9c386a9ed81cc28fb0c4118e4acbbad7107c
Jeremy Muhlich <jmuhlich@bitflood.org>
2015-07-13 16:47:57 -0400

    Improve formatting of comparison

--- src/compare_pysb_sbml.py
+++ src/compare_pysb_sbml.py
@@ -5,20 +5,22 @@ import chen_2009_original_sbml
 import chen_2009
 
 ignore = ('_i', '_h', 'Inh')
-sbml_species = sorted([s.label for s in
-                       chen_2009_original_sbml.get_model().species
-                       if not any(i in s.label for i in ignore)])
-pysb_species = sorted(chen_2009.get_species_names())
+sbml_species = sorted([s for s in chen_2009_original_sbml.get_model().species
+                       if not any(i in s.label for i in ignore)],
+                      key=lambda x: x.label)
+sbml_names = [s.label for s in sbml_species]
+pysb_names = sorted(chen_2009.get_species_names())
 
-fmt = '%-51s\t%-51s'
+fmt = '%-60s\t%-51s'
 print fmt % ('SBML', 'PySB')
-print fmt % (('=' * 51,) * 2)
+print fmt % (('=' * 50,) * 2)
 print
-sm = difflib.SequenceMatcher(None, sbml_species, pysb_species)
+sm = difflib.SequenceMatcher(None, sbml_names, pysb_names)
 for tag, i1, i2, j1, j2 in sm.get_opcodes():
     if tag == 'delete':
-        for s in sbml_species[i1:i2]:
-            print fmt % (s, '')
+        for si in xrange(i1, i2):
+            ss = '%s - %s' % (sbml_names[si], sbml_species[si].name)
+            print fmt % (ss, '')
     elif tag == 'insert':
-        for s in pysb_species[j1:j2]:
+        for s in pysb_names[j1:j2]:
             print fmt % ('', s)
