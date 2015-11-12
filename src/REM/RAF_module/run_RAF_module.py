8404578266bc471dd32aaadd13074f22347bc432
subkar <sub.kartik@gmail.com>
2015-11-12 16:45:12 -0500

    1. Instantiate EGF_to_Ras module 2. Loop over SOS amounts instead of KRAS amount 3. Left panel of figure now shows KRAS:GTP instead of active BRAF. Y-axis is now SOS level

--- src/REM/RAF_module/run_RAF_module.py
+++ src/REM/RAF_module/run_RAF_module.py
@@ -4,10 +4,10 @@ from pysb import *
 
 Model()
 
-# import EGFR_to_RAS
-# EGFR_to_RAS.monomers()
-# EGFR_to_RAS.KRAS_activation()
-# EGFR_to_RAS.declare_observables()
+import EGFR_to_RAS
+EGFR_to_RAS.monomers()
+EGFR_to_RAS.KRAS_activation()
+EGFR_to_RAS.declare_observables()
 
 import BRAF_module
 BRAF_module.monomers()
@@ -24,23 +24,23 @@ ERK_phosphorylation.declare_observables()
 import numpy as np
 from pysb.integrate import Solver
 
-Ras_range = 2 * np.linspace(0, 1000, num=20)
-sos_range = 2 * np.linspace(0, 1000, num=5)
-Vemurafenib_range = 2 * np.linspace(0, 1000, num=20)
+Ras_range = 2 * np.linspace(0, 1000, num=5)
+sos_range = 2 * np.linspace(0, 1000, num=10)
+Vemurafenib_range = 2 * np.linspace(0, 1000, num=10)
 
 RAF_WT_level = []
 RAF_V600E_level = []
 ERK_P_level = []
+KRAS_level = []
 
-for r in Ras_range:
-    # for s in sos_range:
+for s in sos_range:
     wt_holder_row = []
     v600e_holder_row = []
     erk_holder_row = []
+    kras_holder_row = []
     for v in Vemurafenib_range:
 
-        model.parameters['KRAS_0'].value = r
-        # model.parameters['SOS_0'].value = s
+        model.parameters['SOS_0'].value = s
         model.parameters['Vem_0'].value = v
 
         ts = np.linspace(0, 100, 100)
@@ -50,14 +50,17 @@ for r in Ras_range:
         wt_holder_row.append(solver.yobs['BRAF_WT_active'][-1])
         v600e_holder_row.append(solver.yobs['BRAF_V600E_active'][-1])
         erk_holder_row.append(solver.yobs['ERK_P'][-1])
+        kras_holder_row.append(solver.yobs['active_KRAS'][-1])
 
     RAF_WT_level.append(wt_holder_row)
     RAF_V600E_level.append(v600e_holder_row)
     ERK_P_level.append(erk_holder_row)
+    KRAS_level.append(kras_holder_row)
 
 wt = np.array(RAF_WT_level, dtype='float')
 v600e = np.array(RAF_V600E_level, dtype='float')
 erkp = np.array(ERK_P_level, dtype='float')
+kras = np.array(KRAS_level, dtype='float')
 
 # Plots
 # -----
@@ -66,16 +69,17 @@ import matplotlib.pyplot as plt
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
 # wt_c = ax1.pcolor(Vemurafenib_range, Ras_range, wt, vmin=0)
-mut_c = ax1.pcolor(Vemurafenib_range, Ras_range, v600e, vmin=0)
-erk_p = ax2.pcolor(Vemurafenib_range, Ras_range, erkp, vmin=0)
+# mut_c = ax1.pcolor(Vemurafenib_range, sos_range, v600e, vmin=0)
+kras_act = ax1.pcolor(Vemurafenib_range, sos_range, kras, vmin=0)
+erk_p = ax2.pcolor(Vemurafenib_range, sos_range, erkp, vmin=0)
 
 ax1.set_xlabel('Vemurafenib')
 ax2.set_xlabel('Vemurafenib')
-ax1.set_ylabel('Ras')
-ax1.set_title('BRAF_V600E')
+ax1.set_ylabel('SOS')
+ax1.set_title('KRAS:GTP')
 ax2.set_title('ERK_P')
 
-plt.colorbar(mut_c, ax=ax1)
+plt.colorbar(kras_act, ax=ax1)
 plt.colorbar(erk_p, ax=ax2)
 
 plt.show()
