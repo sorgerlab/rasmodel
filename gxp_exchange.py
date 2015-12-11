5771a1fa4dc6b0a6dfbd84ebda2330d4939078b7
John Bachman <bachmanjohn@gmail.com>
2015-12-11 16:10:49 -0500

    Switch open/closed, add gap site to Initial conds

--- gxp_exchange.py
+++ gxp_exchange.py
@@ -15,24 +15,24 @@ KRAS = model.monomers['KRAS']
 GDP = model.monomers['GDP']
 GTP = model.monomers['GTP']
 
-Expression('KRAS_mGXP_', model.observables['KRAS_mGTP_open_'] +
-                         model.observables['KRAS_mGDP_open_'])
+Expression('KRAS_mGXP_', model.observables['KRAS_mGTP_closed_'] +
+                         model.observables['KRAS_mGDP_closed_'])
 
 # Add an initial condition for HRAS with GDP or GTP pre-bound
 # (Concentration units in nM)
-Initial(KRAS(gtp=1, gef=None, p_loop=None, s1s2='open', CAAX=None,
+Initial(KRAS(gtp=1, gap=None, gef=None, p_loop=None, s1s2='closed', CAAX=None,
              mutant='WT') % GDP(p=1, label='n'),
              Parameter('KRAS_WT_GDP_0', 0.))
-Initial(KRAS(gtp=1, gef=None, p_loop=None, s1s2='open', CAAX=None,
+Initial(KRAS(gtp=1, gap=None, gef=None, p_loop=None, s1s2='closed', CAAX=None,
              mutant='G13D') % GDP(p=1, label='n'),
              Parameter('KRAS_G13D_GDP_0', 0.))
-Initial(KRAS(gtp=1, gef=None, p_loop=None, s1s2='open', CAAX=None,
+Initial(KRAS(gtp=1, gap=None, gef=None, p_loop=None, s1s2='closed', CAAX=None,
              mutant='WT') % GTP(p=1, label='n'),
              Parameter('KRAS_WT_GTP_0', 0.))
-Initial(KRAS(gtp=1, gef=None, p_loop=None, s1s2='open', CAAX=None,
+Initial(KRAS(gtp=1, gap=None, gef=None, p_loop=None, s1s2='closed', CAAX=None,
              mutant='G13D') % GTP(p=1, label='n'),
              Parameter('KRAS_G13D_GTP_0', 0.))
-#model.parameters['k_recycle_gtp_from_gdp'].value = 0
+
 plt.ion()
 
 # First simulate the data from Figure 1A (GDP exchange)
@@ -45,26 +45,16 @@ sol.run()
 plt.figure()
 plt.plot(t, sol.yexpr['KRAS_mGXP_'], label='WT')
 
-k = fitting.Parameter(1.)
-A = fitting.Parameter(100.)
-def expfunc(t):
-    return A() * (1 - np.exp(-k()*t))
-res = fitting.fit(expfunc, [A, k], sol.yexpr['KRAS_mGXP_'], t)
-print A(), k()
-
 # G13D, GDP:
 model.parameters['KRAS_WT_GDP_0'].value = 0
 model.parameters['KRAS_G13D_GDP_0'].value = 750.
 sol.run()
 plt.plot(t, sol.yexpr['KRAS_mGXP_'], label='G13D')
 plt.legend(loc='lower right')
-
-k = fitting.Parameter(1.)
-A = fitting.Parameter(100.)
-def expfunc(t):
-    return A() * (1 - np.exp(-k()*t))
-res = fitting.fit(expfunc, [A, k], sol.yexpr['KRAS_mGXP_'], t)
-print A(), k()
+plt.title('GDP exchange')
+plt.xlabel('Time (s)')
+plt.ylabel('[Bound mGDP] (nM)')
+plt.show()
 
 # Now simulate the data from Figure 1B (GTP exchange)
 # WT, GTP
@@ -75,6 +65,8 @@ model.parameters['KRAS_G13D_GDP_0'].value = 0.
 model.parameters['KRAS_WT_GTP_0'].value = 750.
 model.parameters['KRAS_G13D_GTP_0'].value = 0.
 sol.run()
+
+plt.figure()
 plt.plot(t, sol.yexpr['KRAS_mGXP_'], label='WT')
 
 # G13D, GTP
@@ -83,10 +75,7 @@ model.parameters['KRAS_G13D_GTP_0'].value = 750.
 sol.run()
 plt.plot(t, sol.yexpr['KRAS_mGXP_'], label='G13D')
 plt.legend(loc='lower right')
-
-k = fitting.Parameter(1.)
-A = fitting.Parameter(100.)
-def expfunc(t):
-    return A() * (1 - np.exp(-k()*t))
-res = fitting.fit(expfunc, [A, k], sol.yexpr['KRAS_mGXP_'], t)
-print A(), k()
+plt.title('GTP exchange')
+plt.xlabel('Time (s)')
+plt.ylabel('[Bound mGTP] (nM)')
+plt.show()
